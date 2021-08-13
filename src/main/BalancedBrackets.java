2441f42d272be507d9ac5b7ef03405fe5a1635af
vaishnavi <vaishu.srini15@gmail.com>
2021-08-12 20:06:46 -0500

    Modified and added Tests

--- src/main/BalancedBrackets.java
+++ src/main/BalancedBrackets.java
@@ -32,4 +32,5 @@ public class BalancedBrackets {
         }
         return brackets == 0;
     }
+
 }
--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -12,5 +13,46 @@ public class BalancedBracketsTest {
         assertEquals(true, true);
     }
 
-
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+   @Test
+    public void onlyBracketsReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]"));
+   }
+    @Test
+    public void onlyBracketOppositeReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+    @Test
+    public void onlyBracketsNull(){
+        BalancedBrackets NullResult = new BalancedBrackets();
+        assertEquals(0,0);
+    }
+    @Test
+    public void wordReturnValue(){
+        BalancedBrackets Value = new BalancedBrackets();
+        BalancedBrackets.hasBalancedBrackets("[skyline]");
+        int result = 7;
+        assertEquals(result,7);
+    }
+    @Test
+    public void withUnBalancedBrackets(){
+        BalancedBrackets Value = new BalancedBrackets();
+        BalancedBrackets.hasBalancedBrackets("[skyline]");
+        assertFalse(BalancedBrackets.hasBalancedBrackets("skyline["));
+    }
+    @Test
+    public void withNoBrackets(){
+        BalancedBrackets Value = new BalancedBrackets();
+        BalancedBrackets.hasBalancedBrackets("skyline");
+        assertTrue(BalancedBrackets.hasBalancedBrackets("skyline"));
+    }
+    @Test
+    public void withMiddleBrackets(){
+        BalancedBrackets Value = new BalancedBrackets();
+        BalancedBrackets.hasBalancedBrackets("sky[line]");
+        assertTrue(BalancedBrackets.hasBalancedBrackets("sky[line]"));
+    }
 }
